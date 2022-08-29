5708a7696899ad1868172d065a034396d3bcacf9
Min <biffboffkev@gmail.com>
2022-08-28 22:35:02 -0400

    commit tree fe857f086839670956a260d666191b29455039bd

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,15 @@
 import React from "react";
+import Links from './Links.js';
 
-function About() {
+function About(props) {
+  
+
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      <p>{props.user.bio}</p>
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+      <Links github={props.user.links.github} linkedin={props.user.links.linkedin}/>
     </div>
   );
 }
---
+++ src/components/Links.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Links(props) {
+    const {github, linkedin} = props;
+  return (
+    <div id="home">
+        <h3>Links</h3>
+        <a href={github}>{github}</a>
+        <a href={linkedin}>{linkedin}</a>
+    </div>
+  );
+}
+
+export default Links;
